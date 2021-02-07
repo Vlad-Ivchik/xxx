46fcbcac75a6d256ce1ab3f270f093f3d811a1e0
Vlad <ivchik.vlad@gmail.com>
2021-02-07 15:01:32 +0300

    homework 402

--- src/tasks/lesson04/task402.py
+++ src/tasks/lesson04/task402.py
@@ -1,15 +1,17 @@
-a = 0
+def main():
+    a = 0
+    while 1:
+        i = input("number: ")
+        if i == "stop":
+            break
 
-while 1:
-    i = input("number: ")
-    if i == "stop":
-        break
+        if not i.isnumeric():
+            print("wrong input")
+            continue
+        else:
+            n = int(i)
+            a += n
+    return a
 
-    if not i.isnumeric():
-        print("wrong input")
-        continue
-    else:
-        n = int(i)
-        a += n
-
-print(a)
+if __name__ == "__main__":
+    print(main())
