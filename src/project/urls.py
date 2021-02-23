105dfe8cc1a71a7de6504e6ea3168c6e06e57d7c
Vlad <ivchik.vlad@gmail.com>
2021-02-23 19:33:45 +0300

    django tasks

--- src/project/urls.py
+++ src/project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.http import HttpResponse, HttpRequest
 from django.urls import path
 
-from main.handlers.index import handler
+from main.handlers.index import handler_django
 from tasks.lesson01 import task103
 from tasks.lesson03 import task301, task302, task303, task304, task305, task306, task307, task309, task310, task311
 
@@ -21,17 +21,17 @@ def xxxx(req):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", handler),
-    path("tasks/lesson01/task103.html", task103.handler),
-    path("tasks/lesson03/task301.html", task301.handler),
-    path("tasks/lesson03/task302.html", task302.handler),
-    path("tasks/lesson03/task303.html", task303.handler),
-    path("tasks/lesson03/task304.html", task304.handler),
-    path("tasks/lesson03/task305.html", task305.handler),
-    path("tasks/lesson03/task306.html", task306.handler), #???
-    path("tasks/lesson03/task307.html", task307.handler), #???
-    path("tasks/lesson03/task309.html", task309.handler),
-    path("tasks/lesson03/task310.html", task310.handler), #???
-    path("tasks/lesson03/task311.html", task311.handler), #???
+    path("", handler_django),
+    path("tasks/lesson01/task103.html/", task103.handler),
+    path("tasks/lesson03/task301.html/", task301.handler),
+    path("tasks/lesson03/task302.html/", task302.handler),
+    path("tasks/lesson03/task303.html/", task303.handler),
+    path("tasks/lesson03/task304.html/", task304.handler),
+    path("tasks/lesson03/task305.html/", task305.handler),
+    path("tasks/lesson03/task306.html/", task306.handler), #???
+    path("tasks/lesson03/task307.html/", task307.handler), #???
+    path("tasks/lesson03/task309.html/", task309.handler),
+    path("tasks/lesson03/task310.html/", task310.handler), #???
+    path("tasks/lesson03/task311.html/", task311.handler), #???
 ]
 
