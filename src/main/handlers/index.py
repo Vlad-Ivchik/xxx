02a44abaad02e015bae3285ab61207387e7243c2
Vlad <ivchik.vlad@gmail.com>
2021-02-25 21:27:32 +0300

    django tasks

--- src/main/handlers/index.py
+++ src/main/handlers/index.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from django.http import HttpRequest, HttpResponse
 from main.custom_types import RequestT
 from main.custom_types import ResponseT
 from main.util import render_template
@@ -13,5 +13,15 @@ def handler(_request: RequestT) -> ResponseT:
     document = render_template(TEMPLATE, context)
 
     response = ResponseT(payload=document)
+
+    return response
+
+
+def handler_django(_request: HttpRequest) -> HttpResponse:
+
+    context = {"random_number": randint(100000, 999999)}
+    document = render_template(TEMPLATE, context)
+
+    response = HttpResponse(document)
 
     return response
