856dd7ff1e627181d0ceeacc614f05f36e75b98a
Vlad <ivchik.vlad@gmail.com>
2021-02-25 21:36:51 +0300

    django tasks

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -1,15 +1,15 @@
 import sentry_sdk
 
 from framework.util.settings import get_setting
-from main.custom_types import RequestT
 from main.handlers import get_handler
 from main.handlers import handle_500
+from main.util import build_request
 
 sentry_sdk.init(get_setting("SENTRY_DSN"), traces_sample_rate=1.0)
 
 
 def application(environ, start_response):
-    request = RequestT(environ)
+    request = build_request(environ)
     handler = get_handler(request)
 
     try:
@@ -17,8 +17,13 @@ def application(environ, start_response):
     except Exception:
         response = handle_500(request)
 
-    status = f"{response.status.value} {response.status.phrase}"
-    headers_list = list(response.headers_items())
-    start_response(status, headers_list)
+    headers = {
+        "Content-Type": response.content_type,
+    }
+
+    start_response(
+        f"{response.status.value} {response.status.phrase}",
+        list(headers.items()),
+    )
 
     yield response.payload.encode()
